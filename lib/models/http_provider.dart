f4a2ace1e41b1ebf084e95a086fe956b0d650c7c
aldoalfians <aldoboetd908@gmail.com>
2023-03-08 15:38:19 +0700

    http post api using provider

--- lib/models/http_provider.dart
+++ lib/models/http_provider.dart
@@ -1,3 +1,21 @@
+import "dart:convert";
+
+import "package:http/http.dart" as http;
 import 'package:flutter/material.dart';
 
-class HttpProvider with ChangeNotifier {}
+class HttpProvider with ChangeNotifier {
+  Map<String, dynamic> _data = {};
+
+  Map<String, dynamic> get data => _data;
+
+  int get jumlahData => _data.length;
+
+  connectAPI(String name, String job) async {
+    Uri url = Uri.parse("https://reqres.in/api/users");
+
+    var res = await http.post(url, body: {"name": name, "job": job});
+
+    _data = json.decode(res.body);
+    notifyListeners();
+  }
+}
--- lib/pages/home_provider.dart
+++ lib/pages/home_provider.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_http/models/http_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomeProvider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dataProvider = Provider.of<HttpProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text("POST - PROVIDER"),
@@ -13,46 +16,54 @@ class HomeProvider extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FittedBox(
-              child: Text(
-                "ID : Belum ada data",
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-            SizedBox(height: 20),
-            FittedBox(child: Text("Name : ", style: TextStyle(fontSize: 20))),
-            FittedBox(
-              child: Text(
-                "Belum ada data",
-                style: TextStyle(
-                  fontSize: 20,
+            Consumer<HttpProvider>(
+              builder: (context, value, child) => FittedBox(
+                child: Text(
+                  (value.data["id"] == null)
+                      ? "ID : Belum ada data"
+                      : "ID : ${value.data["id"]}",
+                  style: TextStyle(fontSize: 20),
                 ),
               ),
             ),
             SizedBox(height: 20),
-            FittedBox(child: Text("Job : ", style: TextStyle(fontSize: 20))),
-            FittedBox(
-              child: Text(
-                "Belum ada data",
-                style: TextStyle(
-                  fontSize: 20,
+            Consumer<HttpProvider>(
+              builder: (context, value, child) => FittedBox(
+                child: Text(
+                  (value.data["name"] == null)
+                      ? "Nama : Belum ada data"
+                      : "Nama : ${value.data["name"]}",
+                  style: TextStyle(fontSize: 20),
                 ),
               ),
             ),
             SizedBox(height: 20),
-            FittedBox(
-                child: Text("Created At : ", style: TextStyle(fontSize: 20))),
-            FittedBox(
-              child: Text(
-                "Belum ada data",
-                style: TextStyle(
-                  fontSize: 20,
+            Consumer<HttpProvider>(
+              builder: (context, value, child) => FittedBox(
+                child: Text(
+                  (value.data["job"] == null)
+                      ? "Pekerjaan : Belum ada data"
+                      : "Pekerjaan : ${value.data["job"]}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Consumer<HttpProvider>(
+              builder: (context, value, child) => FittedBox(
+                child: Text(
+                  (value.data["createdAt"] == null)
+                      ? "Dibuat : Belum ada data"
+                      : "Dibuat : ${value.data["createdAt"]}",
+                  style: TextStyle(fontSize: 20),
                 ),
               ),
             ),
             SizedBox(height: 100),
             OutlinedButton(
-              onPressed: () {},
+              onPressed: () {
+                dataProvider.connectAPI("Aldo", "FrontEnd");
+              },
               child: Text(
                 "POST DATA",
                 style: TextStyle(
