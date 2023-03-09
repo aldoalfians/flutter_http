6eec2ea3330dee141989be29e7960e275d95dfd5
aldoalfians <aldoboetd908@gmail.com>
2023-03-09 14:20:07 +0700

    http get api using stateful

--- lib/models/http_stateful.dart
+++ lib/models/http_stateful.dart
@@ -3,10 +3,18 @@ import "dart:convert";
 import "package:http/http.dart" as http;
 
 class HttpStateful {
-  String? id, name, job, createdAt;
+  String? id, name, job, fullname, email, createdAt, avatar;
 
-  HttpStateful({this.createdAt, this.id, this.name, this.job});
+  HttpStateful(
+      {this.createdAt,
+      this.id,
+      this.fullname,
+      this.email,
+      this.name,
+      this.job,
+      this.avatar});
 
+  // Post
   static Future<HttpStateful> connectAPI(String name, String job) async {
     Uri url = Uri.parse("https://reqres.in/api/users");
 
@@ -20,5 +28,20 @@ class HttpStateful {
       job: data["job"],
       createdAt: data["createdAt"],
     );
+  }
+
+  // Get
+  static Future<HttpStateful> connectAPIGet(String id) async {
+    Uri url = Uri.parse("https://reqres.in/api/users/$id");
+
+    var res = await http.get(url);
+
+    var data = (json.decode(res.body) as Map<String, dynamic>)["data"];
+
+    return HttpStateful(
+        id: data["id"].toString(),
+        fullname: data["first_name"] + " " + data["last_name"],
+        email: data["email"],
+        avatar: data["avatar"]);
   }
 }
--- lib/pages/home_stateful.dart
+++ lib/pages/home_stateful.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_http/models/http_stateful.dart';
 
@@ -12,7 +14,7 @@ class _HomeStatefulState extends State<HomeStateful> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("POST - STATEFUL"),
+        title: Text("GET - STATEFUL"),
       ),
       body: Container(
         width: double.infinity,
@@ -20,6 +22,20 @@ class _HomeStatefulState extends State<HomeStateful> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Container(
+                height: 100,
+                width: 100,
+                child: Image.network(
+                  (dataResponse.avatar == null)
+                      ? "https://www.uclg-planning.org/sites/default/files/styles/featured_home_left/public/no-user-image-square.jpg?itok=PANMBJF-"
+                      : dataResponse.avatar as String,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
             FittedBox(
               child: Text(
                 (dataResponse.id == null)
@@ -32,34 +48,21 @@ class _HomeStatefulState extends State<HomeStateful> {
             FittedBox(child: Text("Name : ", style: TextStyle(fontSize: 20))),
             FittedBox(
               child: Text(
-                (dataResponse.name == null)
+                (dataResponse.fullname == null)
                     ? "Belum ada data"
-                    : "${dataResponse.name}",
+                    : "${dataResponse.fullname}",
                 style: TextStyle(
                   fontSize: 20,
                 ),
               ),
             ),
             SizedBox(height: 20),
-            FittedBox(child: Text("Job : ", style: TextStyle(fontSize: 20))),
+            FittedBox(child: Text("Email : ", style: TextStyle(fontSize: 20))),
             FittedBox(
               child: Text(
-                (dataResponse.job == null)
+                (dataResponse.email == null)
                     ? "Belum ada data"
-                    : "${dataResponse.job}",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            FittedBox(
-                child: Text("Created At : ", style: TextStyle(fontSize: 20))),
-            FittedBox(
-              child: Text(
-                (dataResponse.createdAt == null)
-                    ? "Belum ada data"
-                    : "${dataResponse.createdAt}",
+                    : "${dataResponse.email}",
                 style: TextStyle(
                   fontSize: 20,
                 ),
@@ -68,15 +71,18 @@ class _HomeStatefulState extends State<HomeStateful> {
             SizedBox(height: 100),
             OutlinedButton(
               onPressed: () {
-                HttpStateful.connectAPI("Aldo", "FrontEnd").then((value) {
-                  print(value.name);
-                  setState(() {
-                    dataResponse = value;
-                  });
-                });
+                HttpStateful.connectAPIGet(
+                        (1 + Random().nextInt(10)).toString())
+                    .then(
+                  (value) {
+                    setState(() {
+                      dataResponse = value;
+                    });
+                  },
+                );
               },
               child: Text(
-                "POST DATA",
+                "GET DATA",
                 style: TextStyle(
                   fontSize: 25,
                 ),
@@ -88,3 +94,100 @@ class _HomeStatefulState extends State<HomeStateful> {
     );
   }
 }
+
+
+
+
+// === Using Post === 
+// class _HomeStatefulState extends State<HomeStateful> {
+//   HttpStateful dataResponse = HttpStateful();
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("POST - STATEFUL"),
+//       ),
+//       body: Container(
+//         width: double.infinity,
+//         margin: const EdgeInsets.all(20),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             FittedBox(
+//               child: Text(
+//                 (dataResponse.id == null)
+//                     ? "ID : Belum ada data"
+//                     : "ID : ${dataResponse.id}",
+//                 style: TextStyle(fontSize: 20),
+//               ),
+//             ),
+//             SizedBox(height: 20),
+//             FittedBox(child: Text("Name : ", style: TextStyle(fontSize: 20))),
+//             FittedBox(
+//               child: Text(
+//                 (dataResponse.name == null)
+//                     ? "Belum ada data"
+//                     : "${dataResponse.name}",
+//                 style: TextStyle(
+//                   fontSize: 20,
+//                 ),
+//               ),
+//             ),
+//             SizedBox(height: 20),
+//             FittedBox(child: Text("Job : ", style: TextStyle(fontSize: 20))),
+//             FittedBox(
+//               child: Text(
+//                 (dataResponse.job == null)
+//                     ? "Belum ada data"
+//                     : "${dataResponse.job}",
+//                 style: TextStyle(
+//                   fontSize: 20,
+//                 ),
+//               ),
+//             ),
+//             SizedBox(height: 20),
+//             FittedBox(
+//                 child: Text("Created At : ", style: TextStyle(fontSize: 20))),
+//             FittedBox(
+//               child: Text(
+//                 (dataResponse.createdAt == null)
+//                     ? "Belum ada data"
+//                     : "${dataResponse.createdAt}",
+//                 style: TextStyle(
+//                   fontSize: 20,
+//                 ),
+//               ),
+//             ),
+//             SizedBox(height: 100),
+//             OutlinedButton(
+//               onPressed: () {
+//                 // Post Http
+//                 // HttpStateful.connectAPI("Aldo", "FrontEnd").then((value) {
+//                 //   print(value.name);
+//                 //   setState(() {
+//                 //     dataResponse = value;
+//                 //   });
+//                 // });
+
+//                 // Get Http
+//                 HttpStateful.connectAPIGet(
+//                         (1 + Random().nextInt(10)).toString())
+//                     .then((value) {
+//                   setState(() {
+//                     dataResponse = value;
+//                   });
+//                 });
+//               },
+//               child: Text(
+//                 "POST DATA",
+//                 style: TextStyle(
+//                   fontSize: 25,
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
